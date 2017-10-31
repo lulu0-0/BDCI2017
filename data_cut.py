8fbe022384ee85444f405f295359e058fbd7e3b9
ier005 <ier005@users.noreply.github.com>
2017-10-31 15:09:04 +0800

    cut test

--- data_cut.py
+++ data_cut.py
@@ -4,15 +4,29 @@
 import jieba.posseg as pseg
 import pandas as pd
 
-filename = "./data/train.csv"
-data = pd.read_csv(filename, encoding="utf-8")
-content = data['content-评论内容']
+def cut(content, fn):
+    with open(fn, 'w') as f:
+        for i in range(len(content)):
+            seg_list = pseg.cut(content[i])
+            words = []
+            for word, flag in seg_list:
+                words.append(word + '/' + flag)
+            f.write(' '.join(words) + '\n')
 
-with open('./data/train_cut.txt', 'w') as f:
-    for i in range(len(content)):
-        seg_list = pseg.cut(content[i])
-        words = []
-        for word, flag in seg_list:
-            words.append(word + '/' + flag)
-        f.write(' '.join(words) + '\n')
 
+def cutTrain():
+    filename = "./data/train.csv"
+    data = pd.read_csv(filename, encoding="utf-8")
+    content = data['content-评论内容']
+    cut(content, './data/train_cut.txt')
+    
+
+def cutTest():
+    filename = './data/test.csv'
+    data = pd.read_csv(filename, encoding="utf-8", header=None)
+    data = data.iloc[:,1]
+    cut(data, './data/test_cut.txt')
+
+
+if __name__ == '__main__':
+    cutTest()
